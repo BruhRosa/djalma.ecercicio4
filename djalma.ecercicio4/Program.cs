0e20d3c5c5156946cdf0a0bbc40d00e12d88c813
User <User@DESKTOP-NRDAL1L>
2022-07-06 19:11:24 -0300

    Adicionar arquivos de projeto.

---
+++ djalma.ecercicio4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace djalma.ecercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            for (b = 1; b <= 50;)
+            {
+                Console.WriteLine(b);
+                b++;
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
